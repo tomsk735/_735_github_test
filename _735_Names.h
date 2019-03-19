79aab99e8057edc372fee163bf53b5dd714f1bc4
BIGHERR <45535869+BIGHERR@users.noreply.github.com>
2019-03-19 12:25:49 +0700

    Create _735_Names.h

---
+++ _735_Names.h
@@ -0,0 +1,23 @@
+char *_735_List[] = 
+ {//0123456789012345678901234567
+   "1.Baranov Dmitry",
+   "2.Borisova Catherine",
+   "3.Gaifulina Anastasia",
+   "4.Goncharova Angelica",
+   "5.Dykhova Anna",
+   "6.Zabaturina Irina",
+   "7.Zaitsev Mikhail",
+   "8.Nikolay Koryshev",
+   "9.Kurtova Marina",
+   "10.Latypov Nail",
+   "11.Lysyh Alexey",
+   "12.Mazurok Oleg",
+   "13.Mironov Danila",
+   "14.Osipov Daniel",
+   "15.Popova Daryana",
+   "16.Saryglar Saikho",
+   "17.Saryglar Chechen",
+   "18.Tatarenko Vladislav",
+   "19.Terekhin Alexander",
+   "20.Fedorov Evgeny"
+ };
