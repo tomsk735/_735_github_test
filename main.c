8ffb8fc4ae55c333c8c5d1e36066ae04bff6c59f
MashaSamoylova <mashanedyak@gmail.com>
2019-03-26 11:13:11 +0700

    Add main.c

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct _person {
+    char *name;
+    int num;
+} person;
+
+void sort(person *list, int n) {
+    printf("++++++++++++++++++++++++++++++++++\n");
+    int new_num;
+    for(int i = 0; i < n; i++) {
+        do{
+           new_num = rand()%n;
+        } while(list[new_num].num != -1);
+        list[new_num].num = new_num;
+        printf("%s\n", list[new_num].name);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    const int _735_len = 20;
+    while(1){
+         person _735_list[] = {
+            {"1. Baranov Dmitriy", -1},
+            {"2. ========================", -1},
+            {"3. Gaifulina Anastasia", -1},
+            {"4. Goncharova Anzhelika", -1},
+            {"5. Dyhova Anna", -1},
+            {"6. ========================", -1},
+            {"7. Zaytsev Mikhail Olegovich", -1},
+            {"8. ========================", -1},
+            {"9. Koryshev Nikolay Pavlovich ", -1},
+            {"10. ========================", -1},
+            {"11. ========================", -1},
+            {"12. Mazurok Oleg Olegovich ", -1},
+            {"13. Mironov Danila", -1},
+            {"14. Osipov Daniil Andreevich", -1},
+            {"15. ========================", -1},
+            {"16. ========================", -1},
+            {"17. ========================", -1},
+            {"18. ========================", -1},
+            {"19. ========================", -1},
+            {"20. Fedorov Evgeniy Andreevich", -1},
+        };
+        sort(_735_list, _735_len);
+    }
+    return EXIT_SUCCESS;
+}
