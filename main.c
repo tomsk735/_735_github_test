f20b37f33a7625a507e7b5168b164dd08f8aba77
MashaSamoylova <mashanedyak@gmail.com>
2019-04-02 18:55:51 +0700

    Add bitbanding

--- main.c
+++ main.c
@@ -3,48 +3,56 @@
 #include "main.h"
 #include "mstn_usb.h"
 
-typedef struct _person {
-    char *name;
-    unsigned is_printed: 1;
-} person;
+#define REG0_ADDR 0x20000000
+#define REG0 *((volatile unsigned long *) (REG0_ADDR))
 
-void sort(person *list, int n) {
+void set_bit(int bit_num) {
+    *((volatile unsigned long *)(REG0_ADDR + 0x2000000 + (bit_num<<2))) = 1;
+};
+
+int get_bit(int bit_num) {
+    return *((volatile unsigned long *)(REG0_ADDR + 0x2000000 + (bit_num<<2)));
+};
+
+void sort(char **list, int n) {
+    REG0 = 0;
     int new_num;
     for(int register i = 0; i < n; i++) {
         do {
            new_num = rand()%n;
-        } while(list[new_num].is_printed);
-        list[new_num].is_printed = 1;
-        printf("%s\n", list[new_num].name);
+        } while(get_bit(new_num));
+        set_bit(new_num);
+        printf("%s\n", list[new_num]);
     }
 }
 
 int main(int argc, char *argv[]) {
     while (USB_GetStatus() != PERMITTED);
+    
     const int _735_len = 20;
-    person _735_list[] = {
-       {"1. Baranov Dmitriy", 0},
-       {"2. ========================", 0},
-       {"3. Gaifulina Anastasia", 0},
-       {"4. Goncharova Anzhelika", 0},
-       {"5. Dyhova Anna", 0},
-       {"6. ========================", 0},
-       {"7. Zaytsev Mikhail Olegovich", 0},
-       {"8. ========================", 0},
-       {"9. Koryshev Nikolay Pavlovich ", 0},
-       {"10. ========================", 0},
-       {"11. ========================", 0},
-       {"12. Mazurok Oleg Olegovich ", 0},
-       {"13. Mironov Danila", 0},
-       {"14. Osipov Daniil Andreevich", 0},
-       {"15. ========================", 0},
-       {"16. ========================", 0},
-       {"17. ========================", 0},
-       {"18. ========================", 0},
-       {"19. ========================", 0},
-       {"20. Fedorov Evgeniy Andreevich", 0},
+    char* _735_list[] = {
+       "1. Baranov Dmitriy",
+       "2. ========================",
+       "3. Gaifulina Anastasia",
+       "4. Goncharova Anzhelika",
+       "5. Dyhova Anna",
+       "6. ========================",
+       "7. Zaytsev Mikhail Olegovich",
+       "8. ========================",
+       "9. Koryshev Nikolay Pavlovich ",
+       "10. ========================",
+       "11. ========================",
+       "12. Mazurok Oleg Olegovich ",
+       "13. Mironov Danila",
+       "14. Osipov Daniil Andreevich",
+       "15. ========================",
+       "16. ========================",
+       "17. ========================",
+       "18. ========================",
+       "19. ========================",
+       "20. Fedorov Evgeniy Andreevich",
    };
+    
    sort(_735_list, _735_len);
-   printf("sizeof(person) = %d\n", sizeof(person));
    return EXIT_SUCCESS;
 }
