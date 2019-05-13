7214f088821365de37e1d30a8e0da734ac6aa2ac
dev.humwer@gmail.com <dev.humwer@gmail.com>
2019-05-13 19:58:36 +0700

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 * File              : main.c
 * Compiler          : IAR for ARM 
 * Version           : 6.30.1.3142
-* Last modified     : 08.04.2019
+* Last modified     : 13.05.2019
 * 
 * Author            : Mazurok Oleg
 * Support mail      : dev.humwer@gmail.com
@@ -25,7 +25,7 @@ void reset_bit(int bit_num) {
   *((volatile unsigned long *)(REG0_ADDR + 0x2000000 + bit_num*4)) = 0;
 }
 
-static char bait = 4;
+static char bait = 0;
 
 struct bitField{
   char flag_a : 1; char flag_b : 1;
@@ -36,16 +36,15 @@ struct bitField{
 
 void main(void){
   // Work with Bit Field
-  _bitField.flag_a = 1;
-  _bitField.flag_a = 0;
+  _bitField.flag_c = 1; _bitField.flag_d = 1; // set 12 to field
+  _bitField.flag_c = 0; _bitField.flag_d = 0; // reset to 0
   // Work with Bit Banding
-  set_bit(0);
-  reset_bit(0);
+  set_bit(12); // Set 12th bit
+  reset_bit(12); // Reset 12th bit
   // Handwork with bits
-  
-  bait |= (1<<0); // set 1 for 0th bit
-  bait &=~(1<<0); // set 0 for 0th bit
-  
+  bait |= (1<<2); bait |= (1<<3); // set 12 to var «bait»
+  bait &=~(1<<2); bait &=~(1<<3); // reset var «bait» to 0
+  // Just for check
   bait ^= (1<<0); // (Now, bit 0th = 0) inversion 
   bait ^= (1<<0); // (Now, bit 0th = 1) inversion 
 }
