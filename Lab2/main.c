2e7fa6c6387e42f9e727640f0390bc1ded965b56
dev.humwer@gmail.com <dev.humwer@gmail.com>
2019-06-04 09:40:38 +0700

    Update

--- Lab2/main.c
+++ Lab2/main.c
@@ -12,32 +12,37 @@
 *
 ********************************************************************************/
 
-int func(void){
+int func(void)
+{
   return func();
 }
 
-int func2(int a[5]){
+int func2(int a[5])
+{
   return (a[0]+a[1]+a[2]+a[3]+a[4]);
 }
 
-int func3(int a, int b, int c, int d, int e, int f){
+int func3(int a, int b, int c, int d, int e, int f)
+{
   return (a+b+c+d+e+f);    
 }
 
-void main(void){
-  //func(); // Recursive function (WARNING)
+void main(void)
+{
   
-  //__asm( "MOV R13, #10"); // R13 = 10 (ERROR)
+  //func(); // Отвечает за рекурсивный вызов функции без условия выхода
   
-  //__asm( "MOV R1, #9999\n" "ADD R13, R1"); // R1 = 9999, R13=(R1+R13) (WARNING)
+  //__asm( "MOV R13, #10"); // Попытка напрямую записать значение в SP
   
-  //int sum = func3(16,32,48,64,80,96); // Stack params
+  //__asm( "MOV R1, #9999\n" "ADD R1, R1"); // Изменение значения в SP
   
-  /* 
-  int array[5] = {1,2,3,4,5}; // Stack params
+  //int sum = func3(16,32,48,64,80,96); // Передача параметров в функцию
+  
+  /*
+  int array[5] = {1,2,3,4,5}; // Передача массива в функцию
   func2(array);
   */
   
-  //__asm( "MOV R0, #0x2\n" "MSR CONTROL, R0"); // Two stack pointer
+  //__asm( "MOV R0, #0x2\n" "MSR CONTROL, R0"); // Переключение указателя стека
 }
 
