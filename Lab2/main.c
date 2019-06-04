7b251418bb895f1187f039423393c45b81bd5061
dev.humwer@gmail.com <dev.humwer@gmail.com>
2019-06-04 10:51:19 +0700

    Update main.c and REAME_ru.txt

--- Lab2/main.c
+++ Lab2/main.c
@@ -3,7 +3,7 @@
 * File              : main.c
 * Compiler          : IAR for ARM 
 * Version           : 6.30.1.3142
-* Last modified     : 03.06.2019
+* Last modified     : 04.06.2019
 * 
 * Author            : Mazurok Oleg
 * Support mail      : dev.humwer@gmail.com
@@ -11,7 +11,14 @@
 * Description       : Work with stack
 *
 ********************************************************************************/
-
+// For run to needed code, you must uncomment needed #define
+//#define RECURSIVEFUNC
+//#define CHANGESP
+//#define ADDSP
+//#define ARRAYTOFUNC
+//#define PARAMSTOFUNC
+//#define TURNSP
+  
 int func(void)
 {
   return func();
@@ -29,20 +36,36 @@ int func3(int a, int b, int c, int d, int e, int f)
 
 void main(void)
 {
-  
-  //func(); // Recursive function (WARNING)
-  
-  //__asm( "MOV R13, #10"); // R13 = 10 (ERROR)
-  
-  //__asm( "MOV R1, #9999\n" "ADD R1, R1"); // R1 = 9999, R13=(R1+R13) (WARNING)
-  
-  //int sum = func3(16,32,48,64,80,96); // Stack params
-  
-  /*
-  int array[5] = {1,2,3,4,5}; // Stack params
-  func2(array);
-  */
-  
-  //__asm( "MOV R0, #0x2\n" "MSR CONTROL, R0"); // Two stack pointer
+  #ifdef RECURSIVEFUNC
+  {
+    func(); // Recursive function (WARNING)
+  }
+  #endif
+  #ifdef CHANGESP
+  {
+    __asm( "MOV R13, #10"); // R13 = 10 (ERROR)
+  }
+  #endif
+  #ifdef ADDSP
+  {
+    __asm( "MOV R1, #9999\n" "ADD R1, R1"); // R1 = 9999, R13=(R1+R13) (WARNING)
+  }
+  #endif
+  #ifdef ARRAYTOFUNC
+  {
+    int sum = func3(16,32,48,64,80,96); // Stack params
+  }
+  #endif
+  #ifdef PARAMSTOFUNC
+  {
+    int array[5] = {1,2,3,4,5}; // Stack params
+    func2(array);
+  }
+  #endif
+  #ifdef TURNSP
+  {
+    __asm( "MOV R0, #0x2\n" "MSR CONTROL, R0"); // Two stack pointer
+  }
+  #endif
 }
 
